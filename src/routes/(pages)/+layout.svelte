3c667fb6455e4bc6433189f69e510e72fc50d341
awaisamjad <awaisamjad08@gmail.com>
2025-01-08 14:49:41 +0000

    new layout using snippets and other stuff works

--- src/routes/(pages)/+layout.svelte
+++ src/routes/(pages)/+layout.svelte
@@ -1,4 +1,14 @@
+<script lang="ts">
+
+	import { initSlots } from "$lib/layout-slots.svelte.js";
+
+	let { children } = $props();
+
+	const slots = initSlots();
+</script>
+
 <div class="grid h-screen grid-rows-[auto_1fr_auto] grid-cols-[1fr_3fr_1fr]">
+	{@render children()}
 	<!-- Navigation -->
   <nav class="col-span-full grid grid-cols-[20fr_60fr_20fr] items-center gap-4 p-4 bg-gray-800 border-b-2 border-text">
 		<!-- Logo -->
@@ -23,14 +33,17 @@
 
 	<!-- Left Sidebar -->
 	<aside class="bg-gray-800 border-r-2 row-start-2 row-end-3 col-start-1 col-end-2">
+		{@render slots.left_sidebar()}
 	</aside>
 
 	<!-- Main Content (Scrollable) -->
 	<main class="bg-gray-700 row-start-2 row-end-3 col-start-2 col-end-3 overflow-y-auto">
-		<slot/>
+		{@render slots.main_content()}
 	</main>
 
 	<!-- Right Sidebar -->
 	<aside class="bg-gray-800 row-start-2 row-end-3 col-start-3 col-end-4 border-l-2">
+		{@render slots.right_sidebar()}
 	</aside>
+
 </div>
