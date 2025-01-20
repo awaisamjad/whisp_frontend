6cca8986b3f0840e76392ac054a27c0fee915683
awaisamjad <awaisamjad08@gmail.com>
2025-01-20 12:45:14 +0000

    Changed auth layout to use h-screen

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -2,7 +2,7 @@
     let { children } = $props();
 </script>
 
-<main class="flex flex-col items-center justify-center">
+<main class="flex flex-col items-center justify-center h-screen">
     <!-- <slot/> -->
     {@render children()}
 </main>
