7360273a6c495687b0c8e977cf5ed0b9bb69f041
awaisamjad <awaisamjad08@gmail.com>
2025-02-06 16:48:32 +0000

    changes

--- src/routes/(pages)/+layout.server.ts
+++ src/routes/(pages)/+layout.server.ts
@@ -1,5 +1,6 @@
 import { BACKEND_LOCAL_URL } from "$env/static/private"
 export const load = async () => {
+    console.log(BACKEND_LOCAL_URL)
     return {
         url: `${BACKEND_LOCAL_URL}/create-post`
     }
